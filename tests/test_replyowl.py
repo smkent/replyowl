83605a0fa6c2e5006c1eaf92363d7eb7cd9571da
Stephen Kent <smkent@smkent.net>
2022-03-01 14:28:28 -0800

    Test with make_{text,html} in both states each

--- tests/test_replyowl.py
+++ tests/test_replyowl.py
@@ -6,6 +6,8 @@ from .reply_cases import ReplyCases
 
 
 @pytest.mark.parametrize(**ReplyCases.compose_reply().__dict__)
+@pytest.mark.parametrize("make_html", (True, False), ids=["html", "no_html"])
+@pytest.mark.parametrize("make_text", (True, False), ids=["text", "no_text"])
 def test_compose_reply(
     reply_template: str,
     received_html: str,
@@ -13,10 +15,17 @@ def test_compose_reply(
     expected_html: str,
     expected_text: str,
     reply_attribution: str,
+    make_text: bool,
+    make_html: bool,
 ) -> None:
-    ro = ReplyOwl()
-    text, html = ro.compose_reply(
-        reply_template, reply_attribution, received_text, received_html
+    owl = ReplyOwl()
+    text, html = owl.compose_reply(
+        content=reply_template,
+        quote_attribution=reply_attribution,
+        quote_text=received_text,
+        quote_html=received_html,
+        make_text=make_text,
+        make_html=make_html,
     )
-    assert text == expected_text
-    assert html == expected_html
+    assert html == (expected_html if make_html else None)
+    assert text == (expected_text if make_text else None)
